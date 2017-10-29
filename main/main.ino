65224ce3007d924d472a46c6a565070425aff4d2
Cameron Napoli <cameronnapoli@gmail.com>
2017-10-29 14:38:26 -0700

    Inializing Arduino main.

---
+++ main/main.ino
@@ -0,0 +1,17 @@
+/*
+ * Laser Integrated Digital Turnstile system
+ * 
+ * October 27, 2017
+ * 
+ * Cameron Napoli
+ * Raymond Wang
+ * Jeremy Quintana
+  */
+
+void setup() {
+
+}
+
+void loop() {
+
+}
