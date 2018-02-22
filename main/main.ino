8e939bc67b6f235f908df1de5129e0e3338b5677
jeremynq <36182435+jeremynq@users.noreply.github.com>
2018-02-21 19:46:59 -0800

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -8,30 +8,44 @@
  * Jeremy Quintana
   */
 
-#include <SoftwareSerial.h> 
-#include <SparkFunESP8266WiFi.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
 
-ESP8266Client client;
+WiFiClient client;
 const char mySSID[] = "JeremyFoundation 2.4";
 const char myPSK[] = "DoctorJoseph123";
 const char server[] = "http://backend-env.xzz7reypjg.us-west-1.elasticbeanstalk.com";
-int sensorA = 2;
-int sensorB = 3;
+unsigned int timeout = 40;
+//D3 = sensorA (enter)
+//D2 = sensorB (exit)
+//D8 = WiFi LED
+
 int oldvalA;
 int oldvalB;
 int newvalA;
 int newvalB;
-                           
+
+
 void errorLoop(int error)
 {
   Serial.print(F("Error: "));
   Serial.println(error);
   Serial.println(F("Looping forever."));
-  for (;;);
+  for (;;)
+  {
+    delay(5);
+  }
 }
 
 void postexitRequest()
 {
+  if (!client.connect(server, 80))
+  {
+    Serial.println("Connection failed.");
+  }
+  else
+  {
   client.println("Post /register_event HTTP\1.1");
   client.println("Host: http://backend-env.xzz7reypjg.us-west-1.elasticbeanstalk.com");
   client.println("Connection: close\n");
@@ -43,11 +57,18 @@ void postexitRequest()
   client.println("\"deviceID\" : 4");
   client.println("</html>");
   Serial.println("Request Sent");
+  }
+  client.stop();
 }
-
 
 void postenterRequest()
 {
+    if (!client.connect(server, 80))
+  {
+    Serial.println("Connection failed.");
+  }
+  else
+  {
   client.println("Post /register_event HTTP\1.1");
   client.println("Host: http://backend-env.xzz7reypjg.us-west-1.elasticbeanstalk.com");
   client.println("Connection: close\n");
@@ -59,91 +80,74 @@ void postenterRequest()
   client.println("\"deviceID\" : 4");
   client.println("</html>");
   Serial.println("Request Sent");
+  }
 }
 
 void setup() 
 {
-  pinMode(sensorA, INPUT);
-  pinMode(sensorB, INPUT);
-  oldvalA = digitalRead(sensorA);
-  oldvalB = digitalRead(sensorB);
-  
-  Serial.begin(9600);
-  while (!Serial);
-  
-  int test = esp8266.begin();
-  if (test!= true)
-  {
-    Serial.println(F("Error talking to ESP8266."));
-   errorLoop(test); 
-  }
-  Serial.println(F("ESP8266 Shield Present"));
-  delay(50);
+ pinMode(D3, INPUT);
+ pinMode(D2, INPUT);
+ pinMode(D8, OUTPUT);
+ oldvalA = digitalRead(D3);
+ oldvalB = digitalRead(D2);
 
-  int retVal = esp8266.getMode();
-  if (retVal != ESP8266_MODE_STA)
-  {
-    retVal = esp8266.setMode(ESP8266_MODE_STA);
-    if (retVal < 0)
-    {
-      Serial.println(F("Error setting mode."));
-      errorLoop(retVal);
-    }
-  }
-  Serial.println(F("Mode set to station."));
-  delay(50);
-  
-  Serial.print(F("Connecting to "));
+ Serial.begin(115200);
+ while (!Serial);
+ Serial.print("Connecting to ");
+ Serial.println(mySSID);
+ WiFi.mode(WIFI_STA);
+ WiFi.begin(mySSID, myPSK);
+ while (WiFi.status() != WL_CONNECTED && (--timeout > 0))
+ {
+  digitalWrite(D8, HIGH);
+  delay(250);
+  digitalWrite(D8, LOW);
+  Serial.print(".");
+  delay(250);
+ }
+ if (WiFi.status() != WL_CONNECTED)
+ {
+  Serial.println(F("Error connecting."));
+  digitalWrite(D8, LOW);
+  errorLoop(WiFi.status());
+ }
+ else if (WiFi.status() == WL_CONNECTED)
+ {
+  Serial.println(" ");
+  Serial.print(F("WiFi connected to: "));
   Serial.println(mySSID);
-  retVal =esp8266.connect(mySSID, myPSK);
-  if (retVal < 0)
-  {
-    Serial.println(F("Error connecting."));
-    errorLoop(retVal);
-  }
-
-  char connectedSSID[24];
-  memset(connectedSSID, 0, 24);
-  retVal = esp8266.getAP(connectedSSID);
-  if (retVal > 0)
-  {
-    Serial.print(F("Connected to: "));
-    Serial.println(connectedSSID);
-  }
-  IPAddress myIP = esp8266.localIP();
-  Serial.print(F("My IP: "));
-  Serial.println(myIP);
-  delay(50);
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  digitalWrite(D8, HIGH);
   
-  retVal = client.connect(server, 80);
-  if (retVal <= 0)
-  {
-    Serial.println(F("Failed to connect to server."));
-    errorLoop(retVal);
-  }
-  Serial.println(F("Connected to Server."));
-  delay(50);
-  Serial.println(F("Ready to Count!"));
-}
+ } 
+ delay(50);
+  Serial.println("Ready to Count!");
+ }
 
 void loop() 
 {
-  newvalA = digitalRead(sensorA);
-  newvalB = digitalRead(sensorB);
+  newvalA = digitalRead(D3);
+  newvalB = digitalRead(D2);
 
   if (oldvalA && !newvalA)
   {
     Serial.println("enter");
     postenterRequest();
-    
-    while (!digitalRead(sensorA) || !digitalRead(sensorB));
+    while (!digitalRead(D3) || !digitalRead(D2))
+    {
+      yield();
+    }
   }
+  
   else if (oldvalB && !newvalB)
   {
     Serial.println("exit");
     postexitRequest();
-    
-    while (!digitalRead(sensorA) || !digitalRead(sensorB));
+    while (!digitalRead(D3) || !digitalRead(D2))
+    {
+      yield();
+    }
   }
   oldvalA = newvalA;
   oldvalB = newvalB;
