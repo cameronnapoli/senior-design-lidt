b55c077316228a3300f69b13163f795681d40f8a
Raymond Wang <raymond@insellerate.com>
2018-03-12 04:45:53 -0700

    wording fixes

--- frontend/app/card/container/card-container.comp.js
+++ frontend/app/card/container/card-container.comp.js
@@ -16,25 +16,19 @@
         vm.totalExitCount = 0;
         vm.totalOccupantCount = 0;
         vm.totalAvailableCount = 0;
+        vm.clientId = 1;
+
         vm.refreshAll = refreshAll;
 
         (function _init() {
             refreshAll();
-            //vm.devices.push({ ID: 'a', EntryCount: 5, ExitCount: 5});
-            //vm.devices.push({ ID: 'b', EntryCount: 5, ExitCount: 5});
-            //vm.devices.push({ ID: 'c', EntryCount: 5, ExitCount: 5});
-            //vm.devices.push({ ID: 'd', EntryCount: 5, ExitCount: 5});
-            //vm.devices.push({ ID: 'e', EntryCount: 5, ExitCount: 5});
-            //vm.devices.push({ ID: 'f', EntryCount: 5, ExitCount: 5});
-            //vm.devices.push({ ID: 'g', EntryCount: 5, ExitCount: 5});
-            //vm.devices.push({ ID: 'h', EntryCount: 5, ExitCount: 5});
-            //vm.devices.push({ ID: 'j', EntryCount: 5, ExitCount: 5});
-            //vm.devices.push({ ID: 'k', EntryCount: 5, ExitCount: 5});
-            //vm.devices.push({ ID: 'l', EntryCount: 5, ExitCount: 5});
+            vm.devices.push({ ID: 'a', EntryCount: 5, ExitCount: 5});
+            vm.devices.push({ ID: 'b', EntryCount: 5, ExitCount: 5});
+            vm.devices.push({ ID: 'c', EntryCount: 5, ExitCount: 5});
         })();
 
         function refreshAll() {
-            dataService.getAllClientDevices(1)
+            dataService.getAllClientDevices(vm.clientId)
                 .then(function (data) {
                     vm.devices = data;
                     vm.devices.map(function (device) {
