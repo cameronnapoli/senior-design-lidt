0a72dc866f2baa5de94e7fae1672d3fa27dcc97e
Raymond Wang <raymond@insellerate.com>
2018-03-16 08:01:08 -0700

    added interval

--- frontend/app/card/container/card-container.comp.js
+++ frontend/app/card/container/card-container.comp.js
@@ -7,9 +7,9 @@
             controller: Controller
         });
 
-    Controller.$inject = ['dataService']
+    Controller.$inject = ['dataService', '$interval']
 
-    function Controller(dataService) {
+    function Controller(dataService, $interval) {
         var vm = this;
         vm.devices = [];
         vm.totalEntryCount = 0;
@@ -22,6 +22,7 @@
 
         (function _init() {
             refreshAll();
+            $interval(refreshAll, 15000);
         })();
 
         function refreshAll() {
@@ -50,7 +51,7 @@
                                 }
                                 vm.totalEntryCount += device[0].Entries;
                                 vm.totalExitCount += device[0].Exits;
-                                vm.totalOccupantCount += vm.totalEntryCount - vm.totalExitCount;
+                                vm.totalOccupantCount = vm.totalEntryCount - vm.totalExitCount;
                                 vm.totalAvailableCount = data.AvailableCount - vm.totalOccupantCount;
                             });
                     });
